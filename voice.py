994af3c86d2ee93d1361e3a2632906f0a93bac40
Raghav <raghavkalyanisundar@gmail.com>
2026-06-29 22:35:28 -0400

    added a working speech chat where I can talk and made it feel like an actual conversation with the Ai with grace periods and longer duration to talk and speech recognized response where it proceeds only after I'm done talking.

--- voice.py
+++ voice.py
@@ -1,33 +1,80 @@
 import pyttsx3
-
 import sounddevice as sd
 import numpy as np
 import speech_recognition as sr
 
 #this function records the given data from speech or microphone input function
 def listen():
-    recognizer =sr.Recognizer()
+    recognizer = sr.Recognizer()
 
-    duration = 5
-    print("Listening...")
+    fs = 44100
+    silence_threshold = 50
+    silence_limit = 3.0
+    max_time = 30
+    chunk_duration=0.4
+    initial_grace_period = 6.0
 
-    audio_data = sd.rec(int(duration*44100), samplerate=44100, channels=1, dtype='int16')
-    sd.wait()
-    audio_bytes=np.array(audio_data,dtype=np.int16).tobytes()
-    audio=sr.AudioData(audio_bytes,44100,2)
+    print("\nListening...")
+    
+    audio_buffer = []
+    silence_time = 0
+    total_time = 0
+    chunk_size = int(0.4 * fs)
 
+    stream = sd.InputStream(
+        samplerate=fs,
+        channels=1,
+        dtype='int16'
+    )
+
+    with stream:
+        while True:
+            chunk, _ = stream.read(chunk_size)
+            audio_buffer.append(chunk)
+            total_time += 0.4
+
+            volume = np.abs(chunk).mean()
+
+            if total_time < initial_grace_period:
+                silence_time =0
+            else:
+                if volume < silence_threshold:
+                    silence_time+=0.4
+                else:
+                    silence_time=0
+
+            if silence_time >= silence_limit:
+                print("Silence detected, stopping.")
+                break
+
+            if total_time >= max_time:
+                print("Max duration reached, stopping.")
+                break
+
+    audio_np = np.concatenate(audio_buffer, axis=0)
+    audio_bytes = audio_np.tobytes()
+    audio_data = sr.AudioData(audio_bytes, fs, 2)
+
+    print("Processing...")
 
     try:
-        text = recognizer.recognize_google(audio)
-        print("You said:",text)
+        text = recognizer.recognize_google(audio_data)
+        print("You said:", text)
         return text
-    # when google couldn't transcribe the speech
+    
+
+    # error detectors that will return a text to figure out the type of error if occured.
+
     except sr.UnknownValueError:
-        print("Sorry I didn't catch that.")
+        print("Speech could not be understood.")
         return ""
-    # occurs after common network or API errors
-    except sr.RequestError:
-        print("Speech recognition service error.")
+
+    except sr.RequestError as e:
+        print("Speech service error:", e)
+        return ""
+
+    except Exception as e:
+        print("Unexpected error:", e)
         return ""
 
 
